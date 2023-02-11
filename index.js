49a687f41bfd4fb734276f95b0d66656257ba16e
easonqq0000 <30045503+Eason0729@users.noreply.github.com>
2023-02-11 21:56:13 +0800

    fix logging

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ import {DNS, Record} from '@google-cloud/dns'
 
 import {publicIpv4, publicIpv6} from 'public-ip';
 
+import pino from 'pino';
+import pretty from 'pino-pretty'
+
+var logger;
 if (process.env.NODE_ENV !== 'production') {
+    logger=pino({ level: 'info' },pretty());
     console.log("using .env file")
     dotenv.config();
+}else{
+    logger=pino({ level: 'debug' },pretty());
 }
+
 
 function main(){
     const zone_name=process.env.ZONE_NAME;
@@ -14,24 +22,29 @@ function main(){
     const ttl=process.env.TTL;
     const detect_interval=process.env.DETECT_INTERVAL;
 
-    console.log("zone_name: ",zone_name);
-    console.log("domain_name: ",domain_name);
-    console.log("ttl: ",ttl);
-    console.log("detect_interval: ",detect_interval);
+    logger.info(`
+server running with config:
+zone_name: ${zone_name}
+domain_name: ${domain_name}
+ttl: ${ttl}
+detect_interval: ${detect_interval}`,);
+
 
     let last_ipv4;
     let last_ipv6;
 
     async function check(){
+        logger.trace(`checking ip`);
         let ipv4=await publicIpv4();
-//        console.log(ipv4);
         if (last_ipv4!=ipv4){
+            logger.debug(`ipv4 changed from ${last_ipv4} to ${ipv4}`);
             await ddns_update("A",ipv4);
         }
         last_ipv4=ipv4;
 
         let ipv6=await publicIpv6();
         if (last_ipv6!=ipv6){
+            logger.debug(`ipv4 changed from ${last_ipv4} to ${ipv4}`);
             await ddns_update("AAAA",ipv6);
         }
         last_ipv6=ipv6;
@@ -41,6 +54,7 @@ function main(){
 }
 
 async function ddns_update(record_type,expect_value){
+    logger.info(`asyncing ${record_type} record`);
     const domain_name=process.env.DOMAIN_NAME;
     const zone_name=process.env.ZONE_NAME;
     const ttl=process.env.TTL;
@@ -52,10 +66,8 @@ async function ddns_update(record_type,expect_value){
     let record=record_list.find((record)=>record.name== domain_name);
 
     if (record){
-        console.log("deleting "+record_type+" ResourceRecordSet");
         await zone.deleteRecords(record);
     }
-    console.log("creating "+record_type+" ResourceRecordSet");
     let new_record=new Record(zone,record_type,{
         data: expect_value,
         name: domain_name,
@@ -63,7 +75,6 @@ async function ddns_update(record_type,expect_value){
     });
     await zone.addRecords(new_record);
 
-    console.log("finished updating "+record_type+" record");
 }
 
 main();
